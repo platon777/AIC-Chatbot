80badc06f7c5375eed4d1076c0cc16093eda4740
Wichley Valentin <68266045+platon777@users.noreply.github.com>
2024-08-19 09:43:48 -0400

    Update aic.py

--- streamlit_app/aic.py
+++ streamlit_app/aic.py
@@ -4,22 +4,38 @@ from time import sleep
 from dotenv import load_dotenv
 import os
 from helper_functions import create_thread, retrieve_thread, create_message, list_messages, create_run
-from openai import OpenAI
-import pyodbc
-import streamlit_authenticator as stauth
-import yaml
-from yaml.loader import SafeLoader
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
 
-
+# Charger les variables d'environnement
 load_dotenv()
 
-# Set OpenAI API key
+# Configurer la clé API OpenAI
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Configurer la connexion au serveur SQL
+server = 'localhost'  
+database = 'DataNet' 
+username = 'wvalentin'  # Remplacez par votre nom d'utilisateur
+password = 'Calibre07*'  # Remplacez par votre mot de passe
+driver = 'ODBC Driver 18 for SQL Server'  # Assurez-vous que le driver est installé
+connection_string = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}'
+engine = create_engine(connection_string)
+Session = sessionmaker(bind=engine)
 
+def check_credentials(username, password):
+    session = Session()
+    try:
+        result = session.execute(
+            text("SELECT * FROM users WHERE username=:username AND password=:password"),
+            {'username': username, 'password': password}
+        ).fetchone()
+        return result is not None
+    finally:
+        session.close()
 
 def creds_entered():
-    if st.session_state["user"].strip() == "admin" and st.session_state["passwd"].strip() == "admin":
+    if check_credentials(st.session_state["user"].strip(), st.session_state["passwd"].strip()):
         st.session_state["authenticated"] = True
     else:
         st.session_state["authenticated"] = False
@@ -30,7 +46,6 @@ def authenticate_user():
         st.text_input(label="Username", value="", key="user", on_change=creds_entered)
         st.text_input(label="Password", value="", key="passwd", type="password", on_change=creds_entered)
         return False
-    
     else: 
         if st.session_state["authenticated"]:
             return True
@@ -38,14 +53,11 @@ def authenticate_user():
             st.text_input(label="Username", value="", key="user", on_change=creds_entered)
             st.text_input(label="Password", value="", key="passwd", type="password", on_change=creds_entered)
             return False
-    
 
-
-if authenticate_user():
-    
+def main():
     st.title('Chatbot AIC Assurance vie')
 
-    # Set your assistant ID here
+    # Définir votre assistant ID ici
     ASSISTANT_ID = 'asst_auOEyBid2GbfeTD4DQIXAR0X'
 
     if 'thread_id' not in st.session_state:
@@ -57,54 +69,43 @@ if authenticate_user():
     else:
         st.session_state.messages = list_messages(st.session_state.thread_id)
 
-
-    
-
-    # Display messages
-    messages  = list(st.session_state.messages) 
+    # Afficher les messages
+    messages = list(st.session_state.messages) 
     for msg in reversed(messages):
         st.chat_message(msg.role).write(msg.content[0].text.value)
-
-
-    #prompt = st.chat_input("Enter your message")
 
     client = openai.OpenAI()
 
     with st.sidebar:
         st.header("Directional Prompts")
         if st.button("Comment faire une cotation pour une personne age de 49 ans."):
-            st.session_state.user_prompt =  "Comment faire une cotation pour une personne age de 49 ans."
+            st.session_state.user_prompt = "Comment faire une cotation pour une personne age de 49 ans."
         if st.button("C'est quoi l'assurance optima."):
             st.session_state.user_prompt = "C'est quoi l'assurance optima."
         if st.button("Quelles sont les examens a faire pour une assurance vie."):
             st.session_state.user_prompt = "Quelles sont les examens a faire pour une assurance vie."
 
+    prompt_typing = st.chat_input("Enter your message")
+
     if 'user_prompt' in st.session_state:
         prompt = st.session_state.user_prompt
         del st.session_state.user_prompt
     else:
-        prompt = st.chat_input("Enter your message")
-
+        prompt = prompt_typing
 
     if prompt:
-
-        #st.session_state.messages.append({"role": "user", "content": prompt})
         st.chat_message("user").write(prompt)
         create_message(st.session_state.thread_id, "user", prompt)
 
-
-
         with client.beta.threads.runs.create_and_stream(
-
-            thread_id = st.session_state.thread_id,
-            assistant_id = ASSISTANT_ID,
-            model = "gpt-4o",
+            thread_id=st.session_state.thread_id,
+            assistant_id=ASSISTANT_ID,
+            model="gpt-4o",
             instructions=prompt
         ) as stream:
             with st.chat_message("assistant"): 
                 response = st.write_stream(stream.text_deltas)
                 stream.until_done()
 
-
-    
-
+#if authenticate_user():
+main()
